a6b14b780049309b8418453d0f2129faaa4cd60e
Sultan <128064384+Sultan-08@users.noreply.github.com>
2023-11-14 18:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", "click",  function () {
+document.addEventListener("keydown",  function () {
     if (started == false) {
         console.log("Game is started");
         started = true;
